7530fe5aeac9d42a743aafc6722e616cccbd50b6
mukesh <mukeshmahajan501@gmail.com>
2020-08-03 05:43:59 +0530

    Added:getDiceNumber()function

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,18 @@
+import java.util.Random;
+
 public class SnakeAndLadder {
 	// initializing variable
 	int postion = 0;
 
-	public static void main(String[] args) {
+	// function to get dice number between 1 to 6
+	public static int getDiceNumber() {
+		Random random = new Random();
+		return random.nextInt(6) + 1;
+	}
 
+	public static void main(String[] args) {
+		int result = getDiceNumber();
+		System.out.println("Dice number: "+result);
 	}
 
 }
